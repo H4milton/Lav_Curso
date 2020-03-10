cab22dcf11f51dfb24ba162f6f1d3da1f403d1b3
H4milton <brauliohamilton@hotmail.com>
2020-03-10 15:51:07 -0600

    Orden de migraciones corregida

---
+++ database/migrations/2020_03_10_214448_create_permiso_rol_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermisoRolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso_rol', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('rol_id');
+            $table->unsignedInteger('permiso_id');
+            $table->timestamps();
+
+            $table->foreign('rol_id','fk_permisoRol_rol')->references('id')->on('rol');
+            $table->foreign('permiso_id','fk_permisoRol_permiso')->references('id')->on('permiso');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso_rol');
+    }
+}
